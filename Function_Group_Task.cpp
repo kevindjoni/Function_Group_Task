f212d3f7f16c9d31c04fae14c040ac09f294dc7c
kevindjoni <kevin.djoni@yahoo.com>
2016-10-03 16:49:18 +0700

    Create Function_Group_Task.cpp

---
+++ Function_Group_Task.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void getJudgeData(double &);
+
+int main()
+{
+	double score1, 
+		   score2,
+		   score3,
+	       score4,
+		   score5;
+	
+	getJudgeData(score1);
+	getJudgeData(score2);
+	getJudgeData(score3);
+	getJudgeData(score4);
+	getJudgeData(score5);
+	
+	calcScore(score1,score2,score3,score4,score5);	   
+	
+	return 0;
+}
+
+void getJudgeData(double &score)
+{
+	do{
+		cout<<"Please enter the judges' score (0-10): ";
+		cin>>score;
+	}
+	while
+		(score < 0 || score > 10);
+	return;
+}
